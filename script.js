756f433bbf2ba327a710a58ccd6a99a6a5b1a9bd
SASCHA <102165823+JSDID@users.noreply.github.com>
2025-07-29 10:33:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 document.querySelector('.toggle__input').addEventListener('change', function () {
     if (this.checked) {
       console.log('Toggle is ON');
-      // Можна додати будь-яку логіку тут
     } else {
       console.log('Toggle is OFF');
     }
